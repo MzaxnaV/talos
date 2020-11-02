a4f8781c258363b455046c3bfb5cbd9f85e580d2
Haider Ali <1337neo@protonmail.comw>
2020-11-02 18:07:08 +0530

    Add event to clean up in certain cases

---
+++ bot/events/left_member.py
@@ -0,0 +1,45 @@
+from telegram.ext import MessageHandler, Filters
+from ..lib.common import  db
+from tinydb import Query
+
+def handle(update, ctx):
+    # This method is inteded as a clean up
+    # If the captcha has not been solved, the welcome message
+    # still remains in the group and must be removed
+    # If the captcha has not been solved, the database entry
+    # still exists and must be removed
+
+    group_id = update.effective_chat.id
+    user_id = update.message.left_chat_member.id
+
+    User = Query()
+
+    # Delete the welcome msg  when user leaves group
+    # This is done in case captcha has not been
+    # solved.
+    result = db.search(
+        (User.group_id == group_id) & ( User.user_id == user_id )
+    )
+    if(len(result) == 0):
+        return None
+
+    try:
+        update.message.bot.delete_message(
+            chat_id=group_id,
+            message_id=result[0]['message_id']
+        )
+    except Exception as e:
+        # Maybe captcha was solved and the message was deleted already?
+        print("Deleting initial message failed: ")
+        print(str(e))
+
+    # Attempt to delete the database entry
+    try:
+        db.remove(
+            (User.group_id == group_id) & ( User.user_id == user_id )
+        )
+    except Exception as e:
+        print("Deleting database  record failed: ")
+        print(str(e))
+
+handler = MessageHandler(Filters.status_update.left_chat_member, handle)
--- bot/init.py
+++ bot/init.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from telegram.ext import Updater
 from .lib import common
-from .events import new_member, start, verify_captcha
+from .events import new_member, start, verify_captcha, left_member
 
 token = os.getenv('BOT_TOKEN')
 rules_uri = os.getenv('RULES_URI')
@@ -15,6 +15,7 @@ dispatcher = updater.dispatcher
 dispatcher.add_handler(new_member.handler)
 dispatcher.add_handler(start.handler)
 dispatcher.add_handler(verify_captcha.handler)
+dispatcher.add_handler(left_member.handler)
 
 updater.start_polling()
 updater.idle()
