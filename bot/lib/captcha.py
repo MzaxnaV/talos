7e83f24d19cf5c04fab5bcf9b6d3a0dc3e4c688b
Haider Ali <1337neo@protonmail.comw>
2020-12-04 21:21:31 +0530

    change question obj to captcha obj and add new shit

--- bot/lib/captcha.py
+++ bot/lib/captcha.py
@@ -3,10 +3,18 @@ from .common import rules
 import random
 
 
-class Question:
-    def __init__(self,  group_id, total_iterations):
+class WrongAnswerError(Exception):
+    pass
+
+
+class Captcha:
+    def __init__(self,  group_id, total_iterations, errors_allowed):
+        self.group_id = group_id
         self.iteration = 0
         self.total_iterations = total_iterations
+        self.errors_allowed = errors_allowed
+        self.solved = 0
+        self.wrong = 0
         self.questions = []
         self.answers = []
         self.choices = []
@@ -14,11 +22,27 @@ class Question:
         for i in range(total_iterations):
             self.generate_question()
 
+    # Sets captcha to the next question
+    # and increments solved counter
     def __add__(self, num):
         self.iteration = self.iteration + num
+        self.solved = self.solved + num
+
+    # Increments wrong counter
+    # Used to count how many wrong attempts were made
+    def __sub__(self, num):
+        self.wrong = self.wrong + num
 
     def __eq__(self, val):
-        return (val == self.answers[self.iteration])
+        if val != self.answers[self.iteration]:
+            raise WrongAnswerError
+        return True
+
+    def has_failed(self):
+        return (self.wrong >= self.errors_allowed)
+
+    def is_solved(self):
+        return (self.solved == self.total_iterations)
 
     def generate_question(self):
         question = random.choice(range(0, len(rules)))
@@ -27,7 +51,7 @@ class Question:
 
         # Ensure its unique
         if answer in self.answers:
-            return self.generate_question(self)
+            return self.generate_question()
 
         while len(choices) < 4:
             rand_choice = random.choice(range(1, len(rules)+1))
@@ -38,15 +62,13 @@ class Question:
         self.questions.append(rules[question])
         self.answers.append(answer)
         self.choices.append(choices)
-        print(self.questions)
-        print(self.answers)
 
     def __str__(self):
         return (f'Question {self.iteration+1}/{self.total_iterations}\n'
                 f'{self.questions[self.iteration]}\n')
 
     def answer_choices(self):
-        callback = 'verify_captcha_{group_id}'
+        callback = f'verify_captcha_{self.group_id}'
         return InlineKeyboardMarkup([
             [
                 InlineKeyboardButton(text=choice,
