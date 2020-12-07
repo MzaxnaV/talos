d934f7b22814de009e9a1d57f737045836da8208
Haider Ali <1337neo@protonmail.comw>
2020-12-07 22:38:01 +0530

    Fix parsing of .env setting to bool/ Optional welcome msg check in new  member event

--- bot/events/new_member.py
+++ bot/events/new_member.py
@@ -1,4 +1,4 @@
-from ..config import BOT_USERNAME, RULES_URI_HUMAN
+from ..config import BOT_USERNAME, RULES_URI_HUMAN, ENABLE_WELCOME_MSG
 from telegram.ext import MessageHandler, Filters
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from ..lib.common import db, mute_perms, get_mention
@@ -12,6 +12,7 @@ def handle(update, ctx):
 
     group_id = update.effective_chat.id
     user_id = update.message.new_chat_members[0].id
+    msgid = None
 
     update.message.bot.restrict_chat_member(
         chat_id=group_id,
@@ -19,31 +20,33 @@ def handle(update, ctx):
         permissions=mute_perms
     )
 
-    url = f't.me/{BOT_USERNAME}?start={update.effective_chat.id}'
-    keyboard = [
-        [
-            InlineKeyboardButton(text='Click to unmute', url=url)
-        ],
-        [
-            InlineKeyboardButton(text='Rules', url=RULES_URI_HUMAN)
+    if ENABLE_WELCOME_MSG:
+        url = f't.me/{BOT_USERNAME}?start={update.effective_chat.id}'
+        keyboard = [
+            [
+                InlineKeyboardButton(text='Click to unmute', url=url)
+            ],
+            [
+                InlineKeyboardButton(text='Rules', url=RULES_URI_HUMAN)
+            ]
         ]
-    ]
-    user_mention = get_mention(update.effective_user)
-    text = (f'Hey there, {user_mention}. You\'ve been muted until you solve a'
-            ' captcha.')
-    msg = update.message.bot.send_message(
-        chat_id=group_id,
-        text=text,
-        reply_markup=InlineKeyboardMarkup(keyboard),
-        parse_mode="MARKDOWN"
-    )
+        user_mention = get_mention(update.effective_user)
+        text = (f'Hey there, {user_mention}. You\'ve been muted until you '
+                'solve a captcha.')
+        msg = update.message.bot.send_message(
+            chat_id=group_id,
+            text=text,
+            reply_markup=InlineKeyboardMarkup(keyboard),
+            parse_mode="MARKDOWN"
+        )
+        msgid = msg.message_id
 
     User = Query()
     db.upsert(
         {
             "group_id": group_id,
             "user_id": user_id,
-            "message_id": msg.message_id
+            "message_id": msgid
         },
         (
             (User.group_id == group_id) & (User.user_id == user_id)
